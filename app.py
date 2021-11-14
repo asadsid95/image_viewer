512d47194d7a5600019c49e165b911dcaf899907
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-11-14 14:19:36 -0500

    found error in path (using .jpeg vs .jpg) + local images needed resizing so using .resize(); #1

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
+
 root = Tk()
 root.title('Image Viewer')
 
-img1 = ImageTk.PhotoImage(Image.open("/images/pic1.jpeg"))
-img2 = ImageTk.PhotoImage(Image.open("/images/pic2.jpeg"))
-img3 = ImageTk.PhotoImage(Image.open("/images/pic3.jpeg"))
+# for filename in os.listdir
+
+
+img1 = ImageTk.PhotoImage(Image.open("images\pic1.jpg").resize((600, 400),Image.ANTIALIAS))
+img2 = ImageTk.PhotoImage(Image.open("images\pic2.jpg").resize((600, 460),Image.ANTIALIAS))
+img3 = ImageTk.PhotoImage(Image.open("images\pic3.jpg").resize((600, 460),Image.ANTIALIAS))
 
 
 next_button = Button(root, text="->")
 back_button = Button(root, text="<-")
 exit_button = Button(root, text="exit", command=root.quit)
 
-
-
 laabel = Label(image=img1)
-laabel.pack()
+laabel.grid(row=0,column=0, columnspan=3)
 
-exit_button.pack()
+back_button.grid(row=1,column=0)
+exit_button.grid(row=1,column=1)
+next_button.grid(row=1,column=2)
+
+# exit_button.pack()
 root.mainloop()                                                                                           
